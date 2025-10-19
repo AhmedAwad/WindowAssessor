08c2a26a02881dd3151011241ba449c0cb50bd01
Ahmed Awad <ahmad.gaafar@gmail.com>
2025-10-19 19:57:47 +0400

    adding missing files

---
+++ storm-runner/src/main/java/storm/bolt/PrintBolt.java
@@ -0,0 +1,22 @@
+package storm.bolt;
+
+// Dumb printer bolt
+import org.apache.storm.topology.base.BaseRichBolt;
+import org.apache.storm.task.OutputCollector;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.tuple.Tuple;
+
+import java.util.Map;
+
+public class PrintBolt extends BaseRichBolt {
+    @Override public void prepare(Map conf, TopologyContext ctx, OutputCollector collector) {}
+    @Override public void execute(Tuple t) {
+        long s = t.getLongByField("winStart");
+        long e = t.getLongByField("winEnd");
+        String k = t.getStringByField("key");
+        long c = t.getLongByField("count");
+        System.out.printf("(%d,%d,%s,%d)%n", s, e, k, c);
+    }
+    @Override public void declareOutputFields(OutputFieldsDeclarer d) {}
+}
